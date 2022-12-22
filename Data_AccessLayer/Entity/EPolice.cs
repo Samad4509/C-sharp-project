5d464c3ed506303cc8a419b29960a2cca623c107
Abdullah-Al- Samad <samad4509@gmail.com>
2022-12-23 02:48:55 +0600

    iportent

---
+++ Data_AccessLayer/Entity/EPolice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_AccessLayer.Entity
+{
+    public class EPolice
+    {
+       public string Name { set; get; }
+       public string Email { set; get; }
+       public string Number { set; get; }
+       public string Password { set; get; }
+       public string Gender { set; get; }
+       public string FatheName { set; get; }
+       public string Address { set; get; }
+       public string Write_Your_Complain { set; get; }
+       public string Crime { set; get; }
+        
+
+
+
+    }
+}
